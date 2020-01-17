65115fa3455731a1918753c38ce765311a6d7eec
Yansey Dominguez <yansey.dc@gmail.com>
2020-01-16 19:32:33 -0800

    get users

--- api/delete/Auth.php
+++ api/delete/Auth.php
@@ -5,8 +5,16 @@
     header('Access-Control-Allow-Headers: application/json'); 
 
     session_start();
+    // remove all session variables
+    session_unset();
+    // destroy the session
     session_destroy();
 
+    // set the expiration date to one hour ago
+    $session_lifetime = time() - 3600;
+    ini_set('session.cookie_lifetime', $session_lifetime);
+    ini_set('session.gc_maxlifetime', $session_lifetime); 
+    
     echo json_encode( 
         array(
         "status" => 200,
--- public/js/index.js
+++ public/js/index.js
@@ -1,38 +1,116 @@
+var objMethods = {
+    "login": {
+        handleAction: handleLogInResponse        
+    },
+    "logout": {
+        handleAction: handleLogOutResponse
+    },
+    "users": {
+        handleAction: handleUsersResponse
+    }
+}
 $(document).ready(function(){
     console.log("im ready");
     initSelectors();
 });
 
 function initSelectors() {
-    $("#login-btn").on("click", handleLogin);
+    $("#login-btn").on("click", handleLogIn);
+    $("#logout-btn").on("click", handleLogOut);
+
+    //if url is list get all the list of users
+    //The pop() method removes the last element of an array, and returns that element.
+    if(window.location.href.split("/").pop() == "list.php") { 
+        handleUsers(); 
+    }
 }
 
 /*
 * Handle Events
 */
 
-function handleLogin() {
-    $.ajax({
+function handleLogIn() {    
+    ajaxCall({
         url: "api/post/Auth.php",
         type: "POST", 
         dataType: "json",
         data: {
-            username: "yansey",
-            password: "ziztemaz"
+            username: $("#username").val(),
+            password: $("#password").val(),
+            isChecked: $("#remember-me").prop('checked'),
         }
-    }).done( function(response, textStatus, jqXHR) {
-        console.log(response);
-    }).fail(function(jqXHR, textStatus, errorThrown) {	        	
-        console.log(textStatus);
-    });
+    }, "login");
 }
 
-function callFunction(params) {	
-	var type = params.type;
-	delete params.type;
-	$.ajax({ url: "", type: "POST", dataType: "json", data: params })
-	.done(hybOvenIncubateBC[type].handleAction)
+function handleLogOut() {    
+    ajaxCall({
+        url: "api/delete/Auth.php",
+        type: "DELETE"        
+    }, "logout");
+}
+
+function handleUsers() {    
+    ajaxCall({
+        url: "api/get/Users.php",
+        type: "GET", 
+        dataType: "json",     
+    }, "users");
+}
+
+/*
+* Handle Events Response
+*/
+
+function handleLogInResponse(data, textStatus, jqXHR) {     
+    if(data.status != 200) {
+        //TODO Error messages
+    } else {
+        window.location = "list.php"
+    }
+}
+
+function handleLogOutResponse(data, textStatus, jqXHR) {    
+    if(data.status != 200) {
+        //TODO Error messages
+    } else {
+        window.location = "login.php"
+    }
+}
+
+function handleUsersResponse(data, textStatus, jqXHR) {    
+    if(data.status != 200) {
+        //TODO Error messages
+    } else {
+        //build tables
+        buildTable(data.data);
+    }
+}
+
+/*
+* commiter functions
+*/
+
+function buildTable(data) {
+    $("#users-table").append('<tbody>')
+    data.forEach(function(user) {
+        $("#users-table").append(`<tr><td> ${user.firstName} ${user.lastName} </td> <td> ${user.group} </td></tr>`)
+    });
+    
+}
+
+/**
+ * Util functions
+ */
+function closeAllNotifications() {
+
+}
+
+function ajaxCall(object, action) {	
+	console.log('ajax call');
+	$.ajax(object)
+	.done(objMethods[action].handleAction)
 	.fail(function(jqXHR, textStatus, errorThrown) {		
-		window.location.href = 'index.jsp';		
+        //TODO Error messages
 	});
 }
+
